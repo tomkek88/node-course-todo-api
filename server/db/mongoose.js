f517aeddb48ee7d359892f28484a1760d88ddadb
Tomasz Brazkiewicz <tom.brazkiewicz@gmail.com>
2018-10-31 11:31:00 +0100

    mlab db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(
+  "mongodb://tomkek:Kanibalek12@ds125272.mlab.com:25272/todoapp"
+);
 
-module.exports = {mongoose};
+module.exports = { mongoose };
