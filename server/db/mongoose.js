3197ec1d0f59a137f20119fcbd9ac9f6b9d8a346
Tomasz Brazkiewicz <tom.brazkiewicz@gmail.com>
2018-10-31 12:28:55 +0100

    mongo uri

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,8 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://localhost:27017/TodoApp');
 mongoose.connect(
-  "mongodb://tomkek:Kanibalek12@ds125272.mlab.com:25272/todoapp"
+  process.env.PROD_MONGODB || "mongodb://localhost:27017/TodoApp"
 );
 
 module.exports = { mongoose };
